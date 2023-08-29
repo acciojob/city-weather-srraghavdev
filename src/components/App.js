2adda3070649b3e84d6051d4c1947138fffbd5a6
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-29 23:07:32 +0530

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,32 @@
 
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 import axios from "axios";
 import 'regenerator-runtime/runtime'
 
 const App = () => {
   var [data,Setdata]=useState({})
-  async function getdata(query){
+  let [term,Setterm]=useState('')
+  useEffect(()=>{
+    (async function getdata(){
     try{
-      let temp=  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=0bbb9949bea044992ad8cfe3859cca57&units=imperial`)
+      let temp=  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=0bbb9949bea044992ad8cfe3859cca57&units=imperial`)
     console.log(temp.data)
     Setdata(temp.data)
+  
   }
     catch(error){
       Setdata({})
       console.log(error)
     }
   }
+    )()
+  },[term])
+  
 
   return (
     <div>
-        <input type='text' className="search" onChange={(event)=>getdata(event.target.value)}></input>
+        <input type='text' className="search" onChange={(event)=>Setterm(event.target.value)} value={JSON.stringify(data).length!=2 ? '' : term}></input>
       { JSON.stringify(data).length!=2 && <div className="weather"> {data.name}
                                           <div>{data.main.temp+'F'}</div>
                                           <div>{data.weather[0].description}</div>
