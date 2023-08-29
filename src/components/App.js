bce154c0cb3fca7367464201f56fba095f014807
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-30 00:57:11 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -13,12 +13,12 @@ const App = () => {
       let temp=  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=0bbb9949bea044992ad8cfe3859cca57&units=imperial`)
     console.log(temp.data)
     Setdata(temp.data)
-    document.getElementsByTagName('input')[0].value=''
+    
   
   }
     catch(error){
       Setdata({})
-
+      document.getElementsByTagName('input')[0].value=''
     }
   }
     )()
@@ -29,9 +29,9 @@ const App = () => {
     <div>
         <input type='text' className="search" onChange={(event)=>Setterm(event.target.value)}></input>
       { JSON.stringify(data).length!=2 && <div className="weather"> {data.name}
-                                          <div>{data.main.temp+'F'}</div>
-                                          <div>{data.weather[0].description}</div>
-                                          <div><img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img></div>
+                                          <div className="weather">{data.main.temp+'F'}</div>
+                                          <div className="weather">{data.weather[0].description}</div>
+                                          <div className="weather"><img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img></div>
       
       
         </div>
